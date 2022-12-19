88454ed6bd79d56e6917c871bb167c36c65fc66c
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-12-19 11:01:25 +0900

    [update] Controller

--- src/main/java/com/likelion/exam/app_2022_12_19/controller/ChatController.java
+++ src/main/java/com/likelion/exam/app_2022_12_19/controller/ChatController.java
@@ -1,16 +1,37 @@
 package com.likelion.exam.app_2022_12_19.controller;
 
+import com.likelion.exam.app_2022_12_19.domain.ChatMessage;
+import com.likelion.exam.app_2022_12_19.domain.dto.ResponseData;
+import com.likelion.exam.app_2022_12_19.domain.dto.WriteMessageRequest;
+import com.likelion.exam.app_2022_12_19.domain.dto.WriteMessageResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
+
+    private final List<ChatMessage> chatMessages = new ArrayList<>();
+
+
     @PostMapping("/writeMessage")
     @ResponseBody
-    public String writeMessage() {
-        return "메시지가 작성되었습니다.";
+    public ResponseData<WriteMessageResponse> writeMessage(@RequestBody WriteMessageRequest request) {
+
+        ChatMessage message = new ChatMessage(request.getAuthorName(), request.getContent());
+
+        chatMessages.add(message);
+
+        return new ResponseData<>("S-1", "메시지가 작성되었습니다.", new WriteMessageResponse(message.getId()));
+        new WriteMessageResponse(message.getId());
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public ResponseData<List<ChatMessage>> messages() {
+        return new ResponseData<>("S-1", "성공", chatMessages);
     }
 }
