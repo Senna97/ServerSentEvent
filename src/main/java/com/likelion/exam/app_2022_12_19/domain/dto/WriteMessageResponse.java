665dc853c27925f5c7690b1b15b05e6a5764bbce
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-12-19 11:00:58 +0900

    [add] 메시지 작성 응답

---
+++ src/main/java/com/likelion/exam/app_2022_12_19/domain/dto/WriteMessageResponse.java
@@ -0,0 +1,10 @@
+package com.likelion.exam.app_2022_12_19.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class WriteMessageResponse {
+    private long id;
+}
